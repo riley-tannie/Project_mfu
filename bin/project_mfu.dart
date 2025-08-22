e3f50059994c09f0247c17d82a5ebd3756e66861
riley-tannie <haxlymalik@gmail.com>
2025-08-22 11:58:22 +0700

    new

--- bin/project_mfu.dart
+++ bin/project_mfu.dart
@@ -1,13 +1,7 @@
 void showAll(){
-  print('x chin dk');
-  print('Bike ll srr dk');
-  print('Eain ll pyn chin dk');
-  print('Hello world');
 }
 
-void totalPaid(){
-  print('Sayar Ko pyw pyy pr pyn lhout pr tok loh');
-}
+void totalPaid(){}
 void main(){
   showAll();
   totalPaid();
