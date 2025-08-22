50d3327a1a0342c2ad1dc755d27069058afc9c78
TheinGi-1 <6631501140@lamduan.mfu.ac.th>
2025-08-22 11:40:54 +0700

    Wanna complete two wishes

--- bin/project_mfu.dart
+++ bin/project_mfu.dart
@@ -1,6 +1,13 @@
-void showAll(){}
+void showAll(){
+  print('x chin dk');
+  print('Bike ll srr dk');
+  print('Eain ll pyn chin dk');
+  print('Hello world');
+}
 
-void totalPaid(){}
+void totalPaid(){
+  print('Sayar Ko pyw pyy pr pyn lhout pr tok loh');
+}
 void main(){
   showAll();
   totalPaid();
