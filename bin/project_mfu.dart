7294f50a993df53666d527524ad446440e02c474
riley-tannie <haxlymalik@gmail.com>
2025-08-22 11:15:28 +0700

    complete template

--- bin/project_mfu.dart
+++ bin/project_mfu.dart
@@ -1,5 +1,7 @@
-import 'package:project_mfu/project_mfu.dart' as project_mfu;
+void showAll(){}
 
-void main(List<String> arguments) {
-  print('Hello world: ${project_mfu.calculate()}!');
+void totalPaid(){}
+void main(){
+  showAll();
+  totalPaid();
 }
